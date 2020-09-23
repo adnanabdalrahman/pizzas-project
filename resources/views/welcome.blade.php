8e31d8e4c7b710c167f7f60daa146cea1363ed6b
adnan.abdalrahman <Open123open>
2020-09-23 13:12:38 +0200

    tenad 13:12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
 <h1>Welcome</h1>
 <p class="msg">{{session('msg')}}</p>
-<a href="/pizzas/create">Order a Pizza 2 4 5</a>
+<a href="/pizzas/create">Order a Pizza 2 4 5 tenado</a>
 <a href="/pizzas/create">this changes from feature1</a>
 
 @endsection
